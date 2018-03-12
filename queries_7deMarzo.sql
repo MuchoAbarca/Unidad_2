1c4708303da398521d2d2f9eaee41275501b0e9b
Ricardo Abarca <ricardo.abarca@cetys.edu.mx>
2018-03-12 16:53:27 -0700

    Update queries_7deMarzo.sql
    En teoría ya funciona el 1, 2, 3, y el 6. 
    
    Revisar el 2, terminar (4-5) y (7-9) antes de miércoles 14

--- queries_7deMarzo.sql
+++ queries_7deMarzo.sql
@@ -1,28 +1,30 @@
 -- Consulta 1.  Obtener los 5 premios nobel mas recientes
-SELECT * FROM NUEVOABARCA.PRIZE (select max year() over (order by));
+SELECT * FROM (SELECT NOMBRE, YEAR, MOTIVACION, ROW_NUMBER () OVER (ORDER BY YEAR DESC) AS LIMIT FROM NUEVOABARCA.PRIZE) WHERE LIMIT < 6;
 
--- Consulta 2.  Obtener los ˙ltimos 10 premios nobel de fÌsica
-SELECT * FROM NUEVOABARCA.PRIZE;
+-- Consulta 2.  Obtener los √∫ltimos 10 premios nobel de f√≠sica
+SELECT * FROM (SELECT NOMBRE, CATEGORY, MOTIVACION, ROW_NUMBER () OVER (ORDER BY YEAR DESC) AS LIMIT FROM NUEVOABARCA.PRIZE PREMIO INNER 
+    JOIN NUEVOABARCA.CATEGO ON CATEGO.CATEGORY_ID = PREMIO.CATEGORY_ID WHERE CATEGO.CATEGORY = 'PHYSICS') WHERE LIMIT < 11;
 
 -- Consulta 3.  Obtener las personas que han ganado mas de un premio
-SELECT * FROM NUEVOABARCA.WINNER;
+SELECT * FROM (SELECT WINNER_ID, COUNT (WINNER_ID) AS WINS FROM WINNER_PRIZE GROUP BY WINNER_ID) WHERE WINS > 1 ORDER BY WINS ASC;
 
 -- Consulta 4.  Obtener el campo o lenguaje con mas premios nobel
-SELECT * FROM NUEVOABARCA.FIELD;
+SELECT * FROM;
 
--- Consulta 5.  Obtener los premios ganados desde el aÒo 1980 al 1990 en quÌmica
-SELECT * FROM NUEVOABARCA.PRIZE;
+-- Consulta 5.  Obtener los premios ganados desde el a√±o 1980 al 1990 en qu√≠mica
+SELECT * FROM;
 
 -- Consulta 6.  Obtener el premio nobel ganado menos ocasiones
-SELECT * FROM NUEVOABARCA.PRIZE;
+SELECT * FROM SELECT NOMBRE, WINS FROM (SELECT NOMBRE, WINS, ROW_NUMBER () OVER 
+  (ORDER BY WINS ASC) AS VECES FOM (SELECT NOMBRE, COUNT(NOMBRE) AS WINS FROM NUEVOABARCA.PRIZE GROUP BY NOMBRE)) WHERE VECES = 1;
 
--- Consulta 7.  Obtener el paÌs con m·s premios nobel
-SELECT * FROM NUEVOABARCA.COUNTRY;
+-- Consulta 7.  Obtener el pa√≠s con m√°s premios nobel
+SELECT * FROM ;
 
 -- Consulta 8.  Obtener los premios nobel compartidos (mas de un ganador en la 
-             -- misma categoria y aÒo)
-SELECT * FROM NUEVOABARCA.PRIZE;
+             -- misma categoria y a√±o)
+SELECT * FROM;
 
 -- Consulta 9.  Obtener el premio de edad de tods los ganadores al momento en 
              -- que ganan su premio
-SELECT * FROM NUEVOABARCA.WINNER;
+SELECT * FROM;
